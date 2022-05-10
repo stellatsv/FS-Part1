1e89242ad608996a2c79d0fc9dccd98c4588146e
Styliani Tsovou <stellatsovou@gmail.com>
2022-05-10 23:13:46 +0300

    ex1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,38 +1,46 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  return (
-    <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3}
-      		exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-  
+	const course = 'Half Stack application development'
+	const part1 = {
+		name: 'Fundamentals of React',
+		exercises: 10
+	}
+	const part2 = {
+		name: 'Using props to pass data',
+		exercises: 7
+	}
+	const part3 = {
+		name: 'State of a component',
+		exercises: 14
+	}
+	return (
+	<div>
+		<Header course={course} />
+		<Content part1={part1} part2={part2} part3={part3}/>
+		<Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+
 	</div>
-  )
+	)
 }
 
 const Header = (props) => {
+	console.log(props)
 	return (<div><h1>{props.course}</h1></div>)
 }
 
 const Content = (props) => {
+	console.log(props)
 	return (<div>
-	<Part name={props.part1} number={props.exercises1}/>
+	<Part name={props.part1.name} number={props.part1.exercises}/>
 	
-	<Part name={props.part2} number={props.exercises2}/>
+	<Part name={props.part2.name} number={props.part2.exercises}/>
 	
-	<Part name={props.part3} number={props.exercises3}/>
+	<Part name={props.part3.name} number={props.part3.exercises}/>
 	</div>
 	)
 }
 
 const Total = (props) => {
+	console.log(props)
 	return (<div>
 	<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
 	</div>
@@ -40,6 +48,7 @@ const Total = (props) => {
 }
 
 const Part = (props) => {
+	console.log(props)
 	return (
 		<div>
 			<p>
