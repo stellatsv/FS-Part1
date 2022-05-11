cdf2f44df9abf24b5042ee731b9ef13756107ee1
Styliani Tsovou <stellatsovou@gmail.com>
2022-05-11 20:33:43 +0300

    ex1.7

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -5,23 +5,38 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const goodClick = () => {
-	const newValue = good + 1
+	  const newValue = good + 1
     console.log('good now', newValue)
     setGood(newValue)
+    calcAverage(newValue, bad, neutral)
+    calcPositive(newValue, bad, neutral)
   }
 
   const badClick = () => {
-	const newValue = bad + 1
-    console.log('bad now', newValue)
+	  const newValue = bad + 1
     setBad(newValue)
+    calcAverage(good, newValue, neutral)
+    calcPositive(good, newValue, neutral)
   }
 
   const neutralClick = () => {
-	const newValue = neutral + 1
+	  const newValue = neutral + 1
     console.log('neutral now', newValue)
     setNeutral(newValue)
+    calcAverage(good, bad, newValue)
+    calcPositive(good, bad, newValue)
+  }
+
+  const calcAverage = (good, bad, neutral) => {
+    setAverage((good - bad) / (good + bad + neutral))
+  }
+
+  const calcPositive = (good, bad, neutral) => {
+    setPositive(good / (good + bad + neutral))
   }
 
   return (
@@ -36,6 +51,8 @@ const App = () => {
 	  <Statistic text="Good" number={good}></Statistic>
 	  <Statistic text="Neutral" number={neutral}></Statistic>
 	  <Statistic text="Bad" number={bad}></Statistic>
+	  <Statistic text="Average" number={average}></Statistic>
+	  <Statistic text="Positive" number={positive}></Statistic>
     </div>
   )
 }
